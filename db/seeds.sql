572396b66cda0b7923ffd4ce4fef9d106d48ceac
bottomshorter <longtopsshortbottoms@gmail.com>
2019-07-02 20:57:53 -0700

    DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Egg Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Great Burger", false);
